e0538f2e9ebc0430be475936a87ca67bdc204f73
Igor Zapletnev <igor.zapletnev@gmail.com>
2021-03-04 12:25:43 +0700

    Custom statusbar color

--- Paperboard/MainInputViewController.swift
+++ Paperboard/MainInputViewController.swift
@@ -33,10 +33,7 @@ class MainInputViewController: MainKeyboardViewController {
     }
     
     override func viewDidLoad() {
-        let statusBar =  UIView()
-        statusBar.frame = UIApplication.shared.statusBarFrame
-        statusBar.backgroundColor = UIColor.white
-        UIApplication.shared.keyWindow?.addSubview(statusBar)
+        setupStatusBar()
         
         clearButton.imageEdgeInsets = UIEdgeInsetsMake(0, 0, 0, 8)
         clearButton.titleEdgeInsets = UIEdgeInsetsMake(0, 8, 0, 0)
@@ -50,6 +47,31 @@ class MainInputViewController: MainKeyboardViewController {
         super.viewDidLoad()
         
         setColorScheme(.light)
+    }
+    
+    func setupStatusBar() {
+        if #available(iOS 13.0, *) {
+            let app = UIApplication.shared
+            let statusBarHeight: CGFloat = app.statusBarFrame.size.height
+            
+            let statusbarView = UIView()
+            statusbarView.backgroundColor = UIColor.white
+            view.addSubview(statusbarView)
+            
+            statusbarView.translatesAutoresizingMaskIntoConstraints = false
+            statusbarView.heightAnchor
+                .constraint(equalToConstant: statusBarHeight).isActive = true
+            statusbarView.widthAnchor
+                .constraint(equalTo: view.widthAnchor, multiplier: 1.0).isActive = true
+            statusbarView.topAnchor
+                .constraint(equalTo: view.topAnchor).isActive = true
+            statusbarView.centerXAnchor
+                .constraint(equalTo: view.centerXAnchor).isActive = true
+            
+        } else {
+            let statusBar = UIApplication.shared.value(forKeyPath: "statusBarWindow.statusBar") as? UIView
+            statusBar?.backgroundColor = UIColor.white
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
