27b45c71382e6ab744a251b0440a4f0631c5834b
Igor Zapletnev <igor.zapletnev@gmail.com>
2021-03-20 16:00:17 +0700

    Center items in the settings

--- Paperboard/Settings/ColumnsViewController.swift
+++ Paperboard/Settings/ColumnsViewController.swift
@@ -35,11 +35,32 @@ class ColumnsViewController: UIViewController {
         
         collectionView.delegate = self
         collectionView.dataSource = self
+        
+        let isLargeMode = UIDevice.current.userInterfaceIdiom == .pad && UIDevice.current.orientation.isLandscape
+        let itemSize = isLargeMode ? CGSize(width: 105, height: 80) : CGSize(width: 80, height: 64)
+        
+        let columnLayout = FlowLayout(
+            itemSize: itemSize,
+            minimumInteritemSpacing: 10,
+            minimumLineSpacing: 10,
+            sectionInset: UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        )
+        collectionView.collectionViewLayout = columnLayout
+        
+        if #available(iOS 11.0, *) {
+            collectionView.contentInsetAdjustmentBehavior = .always
+        }
     }
     
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        collectionView?.collectionViewLayout.invalidateLayout()
+        super.viewWillTransition(to: size, with: coordinator)
+    }
 }
 
 extension ColumnsViewController: UICollectionViewDelegate {
+    
+    
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let prevSel = IndexPath(row: settings.currentColumns - 1, section: 0)
         if let prevCell = collectionView.cellForItem(at: prevSel) as? ColumnItemCell {
@@ -72,23 +93,53 @@ extension ColumnsViewController: UICollectionViewDataSource {
     }
 }
 
-extension ColumnsViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 105, height: 80)
-    }
+class FlowLayout: UICollectionViewFlowLayout {
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 10
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 0
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        if UIDevice.current.orientation.isLandscape {
-            return UIEdgeInsets(top: 0, left: 100, bottom: 0, right: 100)
+    required init(itemSize: CGSize, minimumInteritemSpacing: CGFloat = 0, minimumLineSpacing: CGFloat = 0, sectionInset: UIEdgeInsets = .zero) {
+        super.init()
+        
+        self.itemSize = itemSize
+        self.minimumInteritemSpacing = minimumInteritemSpacing
+        self.minimumLineSpacing = minimumLineSpacing
+        self.sectionInset = sectionInset
+        if #available(iOS 11.0, *) {
+            sectionInsetReference = .fromSafeArea
         }
-        return UIEdgeInsets(top: 0, left: 30, bottom: 0, right: 30)
     }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        let layoutAttributes = super.layoutAttributesForElements(in: rect)!.map { $0.copy() as! UICollectionViewLayoutAttributes }
+        guard scrollDirection == .vertical else { return layoutAttributes }
+        
+        // Filter attributes to compute only cell attributes
+        let cellAttributes = layoutAttributes.filter({ $0.representedElementCategory == .cell })
+        
+        // Group cell attributes by row (cells with same vertical center) and loop on those groups
+        for (_, attributes) in Dictionary(grouping: cellAttributes, by: { ($0.center.y / 10).rounded(.up) * 10 }) {
+            // Get the total width of the cells on the same row
+            let cellsTotalWidth = attributes.reduce(CGFloat(0)) { (partialWidth, attribute) -> CGFloat in
+                partialWidth + attribute.size.width
+            }
+            
+            var totalInset: CGFloat = 0
+            // Calculate the initial left inset
+            if #available(iOS 11.0, *) {
+                totalInset = collectionView!.safeAreaLayoutGuide.layoutFrame.width - cellsTotalWidth - sectionInset.left - sectionInset.right - minimumInteritemSpacing * CGFloat(attributes.count - 1)
+            } else {
+                totalInset = collectionView!.bounds.width - cellsTotalWidth - sectionInset.left - sectionInset.right - minimumInteritemSpacing * CGFloat(attributes.count - 1)
+            }
+            var leftInset = (totalInset / 2 * 10).rounded(.down) / 10 + sectionInset.left
+            
+            // Loop on cells to adjust each cell's origin and prepare leftInset for the next cell
+            for attribute in attributes {
+                attribute.frame.origin.x = leftInset
+                leftInset = attribute.frame.maxX + minimumInteritemSpacing
+            }
+        }
+        return layoutAttributes
+    }    
 }
